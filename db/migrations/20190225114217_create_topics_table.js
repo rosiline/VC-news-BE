0b630845863042702772c180fb5d01721aabbd68
Viktorija Cumacenko <vikuce@gmail.com>
2019-02-25 14:17:06 +0000

    created migration for topics table

--- db/migrations/20190225114217_create_topics_table.js
+++ db/migrations/20190225114217_create_topics_table.js
@@ -1,8 +1,11 @@
 
-exports.up = function(knex, Promise) {
-  
+exports.up = function (knex, Promise) {
+  return knex.schema.createTable('topics', (topicsTable) => {
+    topicsTable.string('slug').primary();
+    topicsTable.string('description').notNullable();
+  });
 };
 
-exports.down = function(knex, Promise) {
-  
+exports.down = function (knex, Promise) {
+  return knex.schema.dropTable('topics');
 };
